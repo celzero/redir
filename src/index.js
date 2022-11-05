0b6252a3ef02082f44aedcd5e50a4c57f28ffa62
Murtaza Aliakbar <murtaza@live.in>
2022-11-05 22:29:24 +0530

    add redir to from translate to weblate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
- const tx = new Map();
- tx.set("sponsor", "https://donate.stripe.com/aEU00s632gus8hyfYZ?prefilled_email=anonymous.donor%40rethinkdns.com");
+const tx = new Map();
+tx.set("sponsor", "https://donate.stripe.com/aEU00s632gus8hyfYZ?prefilled_email=anonymous.donor%40rethinkdns.com");
+tx.set("translate", "https://hosted.weblate.org/engage/rethink-dns-firewall/")
 
 function redirect(r, home) {
   try {
