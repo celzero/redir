80fa4531f7ad0c56490fb27423356d1f09626392
Murtaza Aliakbar <murtaza@live.in>
2025-07-09 22:29:47 +0530

    gauth: introduce GCreds type

--- src/gauth.js
+++ src/gauth.js
@@ -6,10 +6,37 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { buf2b64url, str2byte } from "./buf.js";
-import { rsaSsaSign } from "./webcrypto";
+import { buf2b64url, emptyString, str2byte } from "./buf.js";
+import { rsaSsaSign } from "./webcrypto.js";
 
 // from: gist.github.com/markelliot/6627143be1fc8209c9662c504d0ff205?permalink_comment_id=4177336#gistcomment-4177336
+
+/**
+ * {
+ *  "access_token": "ya29.a0ARrdaM...",
+ *   "expires_in": 3599,
+ *   "token_type": "Bearer"
+ * }
+ */
+export class GCreds {
+  constructor(json) {
+    if (typeof json !== "object" || json == null || json.access_token == null) {
+      throw new TypeError("null or invalid json");
+    }
+    /**
+     * @type {string} - The access token
+     */
+    this.token = json.access_token || null;
+    /**
+     * @type {number} - Unix milliseconds when the token expires
+     */
+    this.expiry = Date.now() + (json.expires_in * 1000 || 0);
+    /**
+     * @type {string} - The type of the token, typically "Bearer"
+     */
+    this.typ = json.token_type || "Bearer";
+  }
+}
 
 /**
  * @param {string} content - The content to sign, typically a JWT header and claimset
@@ -25,9 +52,14 @@ async function sign(content, signingKey) {
  * @param {string} user - service account email
  * @param {string} key - service account private key
  * @param {string[]} scopes - OAuth scopes; ['https://www.googleapis.com/auth/cloud-platform']
- * @returns {Promise<string|null>} - Returns the Google OAuth access token or null on failure
+ * @returns {Promise<GCreds|null>} - Returns the Google OAuth access token or null on failure
  */
 export async function getGoogleAuthToken(user, key, scopes) {
+  if (emptyString(user) || emptyString(key)) {
+    loge("getGoogleAuthToken: invalid user/key parameters");
+    return null;
+  }
+
   const jwtHeader = obj2b64url({ alg: "RS256", typ: "JWT" });
   try {
     const assertiontime = Math.round(Date.now() / 1000);
@@ -57,13 +89,19 @@ export async function getGoogleAuthToken(user, key, scopes) {
       },
       body,
     });
+
+    if (!r.ok) {
+      loge(`getGoogleAuthToken: HTTP ${r.status} ${r.statusText}`);
+      return null;
+    }
+
     // {
     //   "access_token": "ya29.a0ARrdaM...",
     //   "expires_in": 3599,
     //   "token_type": "Bearer"
     // }
-    const { access_token } = await r.json();
-    return access_token;
+    const json = await r.json();
+    return new GCreds(json);
   } catch (err) {
     loge(err.message, err);
     logdir(err);
